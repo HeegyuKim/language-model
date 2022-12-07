6b6955847861ccf65abe11d7347ca45d67905148
HeegyuKim <heekue83@gmail.com>
2022-12-07 06:59:12 +0000

    아 tpu 왜 안 돼!!!!!!!

--- data/generate_for_gpt.py
+++ data/generate_for_gpt.py
@@ -5,30 +5,51 @@ import jsonlines
 from tqdm import tqdm
 
 
-dataset_paths = {
-    # "dialog": [
-    #     "heegyu/aihub_sns_dialog_gpt",
-    #     "heegyu/nikl_online_conv_2022_gpt",
-    # ],
-    "text": [
-        "heegyu/kowikitext",
-        # "heegyu/namuwiki-sentences",
-        ]
-}
+datasets = [
+    # For tiny
+    # "heegyu/kowikitext",
+    # "heegyu/namuwiki-extracted",
+    # "heegyu/aihub_sns_dialog_gpt",
+    "heegyu/nikl_messenger_dialog_gpt",
+    # "heegyu/aihub_spoken_2021",
+
+    # For small
+    # "heegyu/nikl_spoken",
+    # "heegyu/nikl_written",
+    # "heegyu/nia_web",
+    # "heegyu/korean-petitions",
+    "heegyu/nikl_daily_dialog_v1.2",
+
+    # For base
+    "heegyu/nikl_news",
+    
+]
+
+def remove_speaker(x):
+    text = x["text"]
+    for i in range(4):
+        text = text.replace(f"{i} : ", "")
+
+    return {
+        "text": text
+    }
 
 
 class GPTBlockBuilder:
     def load_dataset(self, name):
-        dataset = load_dataset(name, split="train")
-        name_vars = ["sentence", "dialog", "spoken"]
+        ds = load_dataset(name, split="train", use_auth_token=os.environ["HF_TOKEN"])
+        name_vars = ["sentence", "dialog", "spoken", "content", "form"]
 
         for var in name_vars:
-            if var in dataset.column_names:
-                return dataset.rename_column(var, "text")
+            if var in ds.column_names:
+                ds = ds.rename_column(var, "text")
+                if var == "dialog":
+                    ds = ds.map(remove_speaker)
+                break
 
-        return dataset
+        return ds
 
-    def build_block(self, dataset, key: str):
+    def build_block(self, dataset, key: str = 'text'):
         ids = []
         for item in dataset:
             next_ids = self.tokenizer.encode(item[key])
@@ -46,26 +67,25 @@ class GPTBlockBuilder:
              block_size: int,
              output_dir: str = "gpt_block"
              ):
-
+        tokenizer_name = tokenizer.replace("/", "__")
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
         self.bos_token_id = bos_token_id
         self.block_size = block_size
 
-        os.makedirs(output_dir, exist_ok=True)
+        os.makedirs(f"{output_dir}/{tokenizer_name}", exist_ok=True)
 
-        for k, paths in dataset_paths.items():
-            for path in paths:
-                name = path.replace("/", "__")
-                with jsonlines.open(f"{output_dir}/{name}.jsonl", "w") as f:
-                    dataset = self.load_dataset(path)
-                    for block in tqdm(self.build_block(dataset, k), desc=path):
-                        f.write({"ids": block})
+        for path in datasets:
+            name = path.replace("/", "__")
+            with jsonlines.open(f"{output_dir}/{tokenizer_name}/{name}.jsonl", "w") as f:
+                dataset = self.load_dataset(path)
+                for block in tqdm(self.build_block(dataset), desc=path):
+                    f.write({"input_ids": block})
                     
 
 if __name__ == "__main__":
     builder = GPTBlockBuilder()
     builder.main(
-        tokenizer="skt/kogpt2-base-v2",
-        bos_token_id=0,
+        tokenizer="heegyu/kogpt-j-base",
+        bos_token_id=1,
         block_size=1024
     )
