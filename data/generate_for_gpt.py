a769dab3d35c096ab3b487cf3344f54b8004e08a
HeegyuKim <heekue83@gmail.com>
2022-12-13 14:40:45 +0900

    gpt block 생성코드 및 gpu 모델 학습코드

--- data/generate_for_gpt.py
+++ data/generate_for_gpt.py
@@ -1,26 +1,39 @@
 from transformers import AutoTokenizer
-from datasets import load_dataset, interleave_datasets
+from datasets import load_dataset, interleave_datasets, disable_caching
 import os
 import jsonlines
 from tqdm import tqdm
 
 
-dataset_paths = {
-    # "dialog": [
-    #     "heegyu/aihub_sns_dialog_gpt",
-    #     "heegyu/nikl_online_conv_2022_gpt",
-    # ],
-    "text": [
-        "heegyu/kowikitext",
-        # "heegyu/namuwiki-sentences",
-        ]
-}
+disable_caching()
+
+
+dataset_paths = [
+    # For tiny
+    "heegyu/kowikitext",
+    "heegyu/namuwiki-extracted",
+    "heegyu/aihub_sns_dialog_gpt",
+    "heegyu/nikl_messenger_dialog_gpt",
+    "heegyu/aihub_spoken_2021",
+
+    # For small
+    "heegyu/nikl_spoken",
+    "heegyu/nikl_written",
+    "heegyu/nia_web",
+    "heegyu/korean-petitions",
+    "heegyu/nikl_daily_dialog_v1.2",
+
+    # For base
+    "heegyu/nikl_news",
+    "heegyu/nia_book",
+    "Bingsu/KcBERT_Pre-Training_Corpus",
+]
 
 
 class GPTBlockBuilder:
     def load_dataset(self, name):
         dataset = load_dataset(name, split="train")
-        name_vars = ["sentence", "dialog", "spoken"]
+        name_vars = ["sentence", "dialog", "spoken", "document"]
 
         for var in name_vars:
             if var in dataset.column_names:
@@ -28,7 +41,7 @@ class GPTBlockBuilder:
 
         return dataset
 
-    def build_block(self, dataset, key: str):
+    def build_block(self, dataset, key: str = "text"):
         ids = []
         for item in dataset:
             next_ids = self.tokenizer.encode(item[key])
@@ -42,30 +55,31 @@ class GPTBlockBuilder:
 
     def main(self, 
              tokenizer: str, 
-             bos_token_id: int, 
              block_size: int,
              output_dir: str = "gpt_block"
              ):
 
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
-        self.bos_token_id = bos_token_id
+        self.bos_token_id = self.tokenizer.bos_token_id
         self.block_size = block_size
 
         os.makedirs(output_dir, exist_ok=True)
 
-        for k, paths in dataset_paths.items():
-            for path in paths:
-                name = path.replace("/", "__")
-                with jsonlines.open(f"{output_dir}/{name}.jsonl", "w") as f:
-                    dataset = self.load_dataset(path)
-                    for block in tqdm(self.build_block(dataset, k), desc=path):
-                        f.write({"ids": block})
+        for path in dataset_paths:
+            name = path.replace("/", "__")
+            with jsonlines.open(f"{output_dir}/{name}.jsonl", "w") as f:
+                dataset = self.load_dataset(path)
+                for block in tqdm(self.build_block(dataset), desc=path):
+                    f.write({"input_ids": block})
                     
 
 if __name__ == "__main__":
     builder = GPTBlockBuilder()
+    # builder.main(
+    #     tokenizer="heegyu/kogpt-neox-small",
+    #     block_size=1024
+    # )
     builder.main(
-        tokenizer="skt/kogpt2-base-v2",
-        bos_token_id=0,
+        tokenizer="heegyu/kogpt-j-base",
         block_size=1024
     )
--- script/train_gpt_small.sh
+++ script/train_gpt_small.sh
@@ -3,7 +3,7 @@ export WANDB_PROJECT="gpt2"
 MODEL_NAME="heegyu/kogpt-neox-small"
 RUN_NAME="kogpt-neox-small"
 BLOCK_SIZE=512
-BATCH_SIZE=16
+BATCH_SIZE=8
 OVERWRITE=true
 
 python train_clm_pt.py \
@@ -15,9 +15,12 @@ python train_clm_pt.py \
     --gradient_accumulation_steps 1 \
     --save_steps 50000 \
     --eval_steps 10000 \
-    --max_steps 500000 \
+    --max_steps 1000000 \
     --fp16 true \
     --block_size $BLOCK_SIZE \
     --deepspeed deepspeed/stage2.json \
     --output_dir "checkpoint/$RUN_NAME" \
-    --overwrite_output_dir $OVERWRITE
+    --resume_from_checkpoint checkpoint/$RUN_NAME/checkpoint-950000 
+    
+    # "checkpoint/$RUN_NAME/checkpoint-300000/global_step0000"
+    # --overwrite_output_dir $OVERWRITE \
