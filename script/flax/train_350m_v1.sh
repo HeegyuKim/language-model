71ff84ef8680c1ffc019ee934c2ae3f79bf4e441
HeegyuKim <heekue83@gmail.com>
2022-12-28 13:39:30 +0000

    350m bfloat16 하면 학습가능 ㅠ

--- script/flax/train_350m_v1.sh
+++ script/flax/train_350m_v1.sh
@@ -1,14 +1,14 @@
 
 export WANDB_PROJECT="gpt2"
 
-RUN_NAME="gpt-j-400m-v1-lr3e-4-batch8-rev1"
+RUN_NAME="gpt-j-350m-v1-lr3e-4-batch8-rev1"
 
 
 python3 train_clm_flax_v2.py \
     --run_name=$RUN_NAME \
     --output_dir="checkpoint/$RUN_NAME" \
-    --tokenizer_name="heegyu/kogpt-j-400m" \
-    --config_name="heegyu/kogpt-j-400m" \
+    --tokenizer_name="heegyu/kogpt-j-350m" \
+    --config_name="heegyu/kogpt-j-350m" \
     --train_file="/data/gpt-data/v1-vocab51k-block1024/*.jsonl" \
     --cache_dir="/data/gpt-data/.cache/" \
     --do_train \
@@ -22,5 +22,5 @@ python3 train_clm_flax_v2.py \
     --adam_beta1="0.9" --adam_beta2="0.98" --weight_decay="0.01" \
     --overwrite_output_dir \
     --num_train_epochs="3" \
-    --logging_steps="500" \
+    --logging_steps="512" \
     --save_steps="100000"
