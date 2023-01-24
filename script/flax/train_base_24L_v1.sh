4e59f7114a94c164dcb5cfc1a438c9c718f396ec
HeegyuKim <heekue83@gmail.com>
2023-01-24 11:41:55 +0000

    ajoublue gpt2 24L script

--- script/flax/train_base_24L_v1.sh
+++ script/flax/train_base_24L_v1.sh
@@ -2,24 +2,25 @@
 
 export WANDB_PROJECT="gpt2"
 
-RUN_NAME="gpt-j-base-v1-24L-lr6e-4-batch4-rev1"
+RUN_NAME="ajoublue-gpt2-base-v1-24L-lr5e-4-batch512-rev3"
 
 
 python3 train_clm_flax_v2.py \
     --run_name=$RUN_NAME \
     --output_dir="checkpoint/$RUN_NAME" \
-    --tokenizer_name="heegyu/kogpt-j-base-24L" \
-    --config_name="heegyu/kogpt-j-base-24L" \
-    --train_file="/data/gpt-data/v1-vocab51k-block1024/*.jsonl" \
-    --cache_dir="/data/gpt-data/.cache/" \
+    --tokenizer_name="heegyu/ajoublue-gpt2-base-24L" \
+    --config_name="heegyu/ajoublue-gpt2-base-24L" \
+    --train_file="/data/v1-vocab51k-block1024/*.jsonl" \
+    --cache_dir="/data/.cache/" \
     --do_train \
     --block_size="1024" \
     --per_device_train_batch_size="4" \
     --per_device_eval_batch_size="4" \
-    --learning_rate="6e-4" \
+    --gradient_accumulation_steps=16 \
+    --learning_rate="5e-4" \
     --warmup_steps="1000" \
     --adam_beta1="0.9" --adam_beta2="0.98" --weight_decay="0.01" \
     --overwrite_output_dir \
     --num_train_epochs="3" \
-    --logging_steps="500" \
-    --save_steps="25000"
+    --logging_steps="2500" \
+    --save_steps="100000"
