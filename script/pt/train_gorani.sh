808eed98e9314d8c513edc523d56000f99ae20ba
HeegyuKim <heekue83@gmail.com>
2023-05-01 04:32:29 +0000

    novel, dialog 학습

--- script/pt/train_gorani.sh
+++ script/pt/train_gorani.sh
@@ -7,21 +7,25 @@ train() {
     MODEL_NAME=$1
     RUN_NAME=$1
     BATCH_SIZE=$2
+    LR=$3
 
     accelerate launch train_torch.py \
         --output_dir "./checkpoint/$TASK" \
         --project $PROJECT \
-        --run_name $RUN_NAME \
+        --run_name "$RUN_NAME-LR$LR-B$((BATCH_SIZE*8))" \
         --do_eval --do_train \
         --model_name_or_path $MODEL_NAME \
         --model_type $MODEL_TYPE \
         --task $TASK \
+        --dtype bfloat16 \
         --per_device_train_batch_size 1 \
+        --per_device_eval_batch_size 1 \
+        --learning_rate $LR \
         --gradient_accumulation_steps $BATCH_SIZE \
         --num_train_epochs 10 \
-        --max_sequence_length 1024 \
+        --max_sequence_length 512 \
         --save_strategy epoch \
         --logging_steps 50
 }
 
-train "heegyu/ajoublue-gpt2-medium" 8
+train "heegyu/ajoublue-gpt2-medium" 32 1e-4
--- script/pt/train_novel.sh
+++ script/pt/train_novel.sh
@@ -1,14 +1,18 @@
 DATASET="heegyu/naver_webnovel"
 TASK="gpt-finetuning"
 MODEL_TYPE="causal-lm"
-# MODEL_NAME="heegyu/ajoublue-gpt2-medium"
-MODEL_NAME="skt/kogpt2-base-v2"
+
+# MODEL_NAME="skt/kogpt2-base-v2"
 
 train() {
-    DATASET="heegyu/naver_webnovel" # "imdb"
-    RUN_NAME="$MODEL_NAME-$DATASET"
+    DATASET="heegyu/naver_webnovel"
     PROJECT="naver-webnovel"
+    MODEL_NAME="heegyu/ajoublue-gpt2-medium"
+    
     BATCH_SIZE=$1
+    LR=$2
+
+    RUN_NAME="ajoublue-gpt2-medium-$LR-$((8 * $BATCH_SIZE))"
 
     accelerate launch train_torch.py \
         --output_dir "./checkpoint/$TASK" \
@@ -20,12 +24,22 @@ train() {
         --task $TASK \
         --dtype bfloat16 \
         --per_device_train_batch_size 1 \
+        --per_device_eval_batch_size 1 \
+        --learning_rate $LR \
         --gradient_accumulation_steps $BATCH_SIZE \
         --dataset_name $DATASET \
         --num_train_epochs 10 \
+        --learning_rate $LR \
         --max_sequence_length 1024 \
-        --save_strategy epoch \
+        --save_strategy no \
         --logging_steps 50
 }
 
-train 8
+train 8 1e-4
+train 16 1e-4
+
+train 8 3e-4
+train 16 3e-4
+
+train 8 5e-5
+train 16 5e-5
